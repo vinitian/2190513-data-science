338e7a92e271b9743a570b348c0971bfdeaa5451
vinitian <140070514+vinitian@users.noreply.github.com>
2024-09-06 21:57:31 +0700

    update 02_dataprep_01 titanic main.py

--- 02_dataprep_01_TitanicDataPrep/main.py
+++ 02_dataprep_01_TitanicDataPrep/main.py
@@ -4,7 +4,7 @@ from student import *
 def main():
     input_string = input().strip()
     # df = pd.read_csv('./titanic_to_student.csv', index_col=0)
-    df = pd.read_csv('for_02_dataprep_01_TitanicDataPrep/titanic_to_student.csv', index_col=0)
+    df = pd.read_csv('02_dataprep_01_TitanicDataPrep/titanic_to_student.csv', index_col=0)
     if input_string == "Q7":
         df.fillna(df.select_dtypes(
             include='number').mean(), inplace=True)
